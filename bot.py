9923bf31166dd5ed0fc929f48890309d87aafe5f
Artem Batalov <a@batalov.me>
2020-11-13 17:33:28 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,76 @@
+import logging
+
+from telegram import Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+from gtables import Parser, User
+from time import sleep
+
+client = Parser("creds.json")
+users_auth = []
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update: Update, context: CallbackContext):
+    """Send a message when the command /start is issued."""
+    tg_user = update.message.from_user
+    for g in client.groups.values():
+        print(g.users)
+        if tg_user.name in g.users.keys():
+            update.message.reply_text('Здравствуйте {}. Вы принадлежите группе {}'.format(g.users[tg_user.name].name, g.users[tg_user.name].group.name))
+            if g.users[tg_user.name] not in users_auth:
+                users_auth.append(g.users[tg_user.name])
+            print(users_auth)
+            break
+    else:
+        update.message.reply_text('Вы не зачислены ни на один курс.')
+
+
+def help_command(update: Update, context: CallbackContext):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("1497024833:AAFaPevEiPS5DDoIMBdCR-YNjDJ8DEXTfXA", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    # Start the Bot
+    updater.start_polling()
+
+    while True:
+        sleep(30)
+        P
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ gtables.py
@@ -0,0 +1,106 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class Parser(gspread.Client):
+    def __init__(self, credentials_file):
+        creds = ServiceAccountCredentials.from_json_keyfile_name(credentials_file, ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"])
+        super().__init__(creds)
+
+    @property
+    def groups(self):
+        return {file['name']: Group(file['name'], self.open(file['name'])) for file in self.list_spreadsheet_files()}
+
+
+class Group:
+    def __init__(self, name, file):
+        self.file = file
+        self.name = name
+
+    @property
+    def users(self):
+        logging = self.file.worksheet("Logging")
+        users = list(zip(logging.col_values(1)[1:], logging.col_values(2)[1:]))
+        return {user[0]: User(self, *user) for user in users}
+
+    @property
+    def themes(self):
+        sheets = []
+        for sheet in self.file.worksheets():
+            if sheet.title != "Logging":
+                sheets.append(Theme(sheet, self))
+        # sheets.pop("Logging")
+        return sheets
+
+
+class User:
+    def __init__(self, group, id, name):
+        self.id = id
+        self.name = name
+        self.group = group
+        self.queue = []
+        self.answered = []
+
+    def add_task(self, task):
+        self.queue.append(task)
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __repr__(self):
+        return '<User: {}, {}>'.format(self.id, self.name)
+
+
+class Theme:
+    def __init__(self, sheet, group):
+        self.all_values = sheet.get_all_values()
+        self.group = group
+
+    @property
+    def params(self):
+        return {'link': self.all_values[0][1],
+                'repetitions_per_week': self.all_values[0][3],
+                'number_of_repetitions': self.all_values[0][5],
+                'wrong_answer_pop': self.all_values[0][7]
+                }
+
+    @property
+    def questions(self):
+        for row in self.all_values[2:]:
+            question_type = row[0]
+            repetition = row[1]
+            question_text = row[2]
+            right_answer = row[3]
+            remain_answers = row[4:]
+            if question_type == "Open":
+                yield OpenQuestion(repetition, question_text, right_answer)
+            elif question_type == "Multiple choice":
+                yield MultipleQuestion(repetition, question_text, right_answer, remain_answers)
+
+
+class Question:
+    pass
+
+
+class MultipleQuestion(Question):
+    def __init__(self, repetition, question_text, right_answer, remain_answers):
+        self.repetition = repetition
+        self.question_text = question_text
+        self.right_answer = right_answer
+        self.remain_answers = remain_answers
+
+    def __repr__(self):
+        return '<MultipleQuestion: {}; Answers {}; Right {}>'.format(self.question_text, self.remain_answers, self.right_answer)
+
+
+class OpenQuestion(Question):
+    def __init__(self, repetition, question_text, right_answer):
+        self.repetition = repetition
+        self.question_text = question_text
+        self.right_answer = right_answer
+
+
+client = Parser("creds.json")
+q = (list(client.groups["SE"].themes[0].questions))
+print(q)
